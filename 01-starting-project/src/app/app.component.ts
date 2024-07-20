1e25f6ced1cce8cb85c87248598e330fb4efc909
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-07-20 17:26:46 -0400

    Passing data from parent component to child component with inputs

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent 
 {
+resultsData?:
+{
+  year: number,
+  interest: number,
+  valueEndOfYear: number,
+  annualInvestment: number,
+  totalInterest: number,
+  totalAmountInvested: number,
+}[];
+
   onCalculateInvestmentResults(data: InvestmentInput) {
     const{initialInvestment, expectedReturn, duration, annualInvestment} = data;
     const annualData = [];
@@ -32,6 +43,6 @@ export class AppComponent
       });
     }
   
-    console.log(annualData);
+    this.resultsData = annualData;
   }
 }
---
+++ 01-starting-project/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+// results = input<{
+    //   year: year,
+    //   interest: interestEarnedInYear,
+    //   valueEndOfYear: investmentValue,
+    //   annualInvestment: annualInvestment,
+    //   totalInterest: totalInterest,
+    //   totalAmountInvested: initialInvestment + annualInvestment * year,
+    // }>()
+@Input() results?: {
+  year: number,
+  interest: number,
+  valueEndOfYear: number,
+  annualInvestment: number,
+  totalInterest: number,
+  totalAmountInvested: number,
+}[];
+}
